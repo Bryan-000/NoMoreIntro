b2c9d739ba5ca7d6e755d5d8879f3ebd4d09c45d
Bryan_-000- <fredayddd321ewq@gmail.com>
2026-08-31 06:07:25 +0100

    maow

--- Plugin.cs
+++ Plugin.cs
@@ -1,27 +1,37 @@
-﻿namespace NoMoreIntro;
+﻿namespace NoIntro;
 
 using BepInEx;
 using HarmonyLib;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Reflection.Emit;
 
 /// <summary> miaow mrrrrp https://www.youtube.com/watch?v=p2_pHD230wY </summary>
 [BepInPlugin("Bryan_-000-.NoMoreIntro", "NoMoreIntro", "1.0.0")]
 public class Plugin : BaseUnityPlugin
 {
-    /// <summary> Do patch all :P </summary>
     public void Awake() =>
-        new Harmony("miaow rawr").PatchAll(GetType());
+        Harmony.CreateAndPatchAll(GetType(), "Bryan_-000-.NoMoreIntro");
 
     /// <summary> Load into the main menu instead of tutorial cuz like meowmeowmeowmeowmeowmeowmmeow </summary>
     [HarmonyTranspiler] [HarmonyPatch(typeof(Bootstrap), "Start")]
     public static IEnumerable<CodeInstruction> LoadIntoMainMenu(IEnumerable<CodeInstruction> instructions)
     {
+        MethodInfo LoadScene = AccessTools.Method(typeof(SceneHelper), "LoadScene");
+
         foreach (CodeInstruction instruction in instructions)
         {
-            if (instruction.Is(OpCodes.Ldstr, "Intro"))
-                instruction.operand = "Main Menu"; // replace "Intro" with "Main Menu"
-            
+            // replace parameters for SceneHelper.LoadScene()
+            if (instruction.Calls(LoadScene))
+            {
+                yield return new(OpCodes.Pop);
+                yield return new(OpCodes.Pop);
+
+
+                yield return new(OpCodes.Ldstr, "Main Menu");
+                yield return new(OpCodes.Ldc_I4_0); // false
+            }
+
             yield return instruction;
         }
     }
