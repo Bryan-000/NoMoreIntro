171a254e91271d7d9d6acfbc69249fa3888eb242
Bryan_-000- <fredayddd321ewq@gmail.com>
2026-09-02 17:10:16 +0100

    add pconfig gui while keeping independence from pconfig

---
+++ PConfigGUI.cs
@@ -0,0 +1,53 @@
+﻿namespace NoMoreIntro;
+
+using BepInEx.Bootstrap;
+using PluginConfig;
+using PluginConfig.API;
+using PluginConfig.API.Decorators;
+using PluginConfig.API.Fields;
+using System.IO;
+using UnityEngine;
+
+public static class PConfigGUI
+{
+    public static PluginConfigurator config;
+    public static StringField StartScene;
+
+    public static void SafeLoad()
+    {
+        try
+        {
+            if (Chainloader.PluginInfos.ContainsKey(PluginConfiguratorController.PLUGIN_GUID))
+                LoadConfig();
+        }
+        catch { }
+    }
+
+    public static void LoadConfig()
+    {
+        config = PluginConfigurator.Create("NoMoreIntro", "Bryan_-000-.NoMoreIntro");
+        config.presetButtonHidden = true; // fuck u
+        config.image = GrabIcon();
+
+        StartScene = new(config.rootPanel, "StartScene", "bryan000.nomoreintro.startScene", "Main Menu", false, false);
+        StartScene.value = Plugin.StartScene.Value;
+
+        ConfigHeader description = new(config.rootPanel, "The level to load into when the game starts.", 14);
+        description.textColor = Color.gray;
+
+        StartScene.onValueChange += data => Plugin.StartScene.Value = data.value;
+    }
+
+    public static Sprite GrabIcon()
+    {
+        using Stream stream = typeof(Plugin).Assembly.GetManifestResourceStream("icon.png");
+        byte[] data = new byte[stream.Length];
+        stream.Read(data, 0, data.Length);
+
+        Texture2D icon_tex = new(0, 0);
+        if (icon_tex.LoadImage(data))
+            return Sprite.Create(icon_tex, new(0, 0, icon_tex.width, icon_tex.height), new(0.5f, 0.5f));
+
+        return null;
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -1,44 +1,27 @@
-﻿namespace NoIntro;
+﻿namespace NoMoreIntro;
 
 using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
-using PluginConfig.API;
-using PluginConfig.API.Fields;
+using PluginConfig;
 using System.Collections.Generic;
-using System.IO;
 using System.Reflection;
 using System.Reflection.Emit;
-using UnityEngine;
+using static BepInEx.BepInDependency;
 
 /// <summary> miaow mrrrrp https://www.youtube.com/watch?v=p2_pHD230wY </summary>
-[BepInIncompatibility(PluginConfig.PluginInfo.PLUGIN_GUID)]
-[BepInPlugin("Bryan_-000-.NoIntro", "NoIntro", "1.0.0")]
+[BepInDependency(PluginConfiguratorController.PLUGIN_GUID, DependencyFlags.SoftDependency)]
+[BepInPlugin("Bryan_-000-.NoMoreIntro", "NoMoreIntro", "1.7.0")]
 public class Plugin : BaseUnityPlugin
 {
-    public static PluginConfigurator config;
-    public static StringField StartScene;
-
-    public static Sprite GrabIcon()
-    {
-        using Stream stream = typeof(Plugin).Assembly.GetManifestResourceStream("icon.png");
-        byte[] data = new byte[stream.Length];
-        stream.Read(data, 0, data.Length);
-
-        Texture2D icon_tex = new(0, 0);
-        if (icon_tex.LoadImage(data))
-            return Sprite.Create(icon_tex, new(0, 0, icon_tex.width, icon_tex.height), new(0.5f, 0.5f));
-
-        return null;
-    }
+    public static ConfigEntry<string> StartScene;
 
     public void Awake()
     {
-        config = PluginConfigurator.Create("NoIntro", "Bryan_-000-.NoIntro");
-        config.presetButtonHidden = true; // fuck u
-        config.image = GrabIcon();
-        StartScene = new(config.rootPanel, "Start Scene", "startscene", "Main Menu");
+        StartScene = Config.Bind("Settings", "startscene", "Main Menu", "The level to load into when the game starts.");
+        PConfigGUI.SafeLoad();
 
-        Harmony.CreateAndPatchAll(GetType(), "Bryan_-000-.NoIntro");
+        Harmony.CreateAndPatchAll(GetType(), "Bryan_-000-.NoMoreIntro");
     }
 
     /// <summary> Load into the specified startscene instead of intro/tutorial cuz like meowmeowmeowmeowmeowmeowmmeow </summary>
@@ -56,7 +39,7 @@ public class Plugin : BaseUnityPlugin
                 yield return new(OpCodes.Pop);
 
 
-                yield return new(OpCodes.Ldstr, StartScene.value);
+                yield return new(OpCodes.Ldstr, StartScene.Value);
                 yield return new(OpCodes.Ldc_I4_0); // false
             }
 
